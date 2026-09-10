15f35b8fb1995713bdd8d03f9f1aa03cda372cdc
Ismail Ferdi <elfernago3hhhh@gmail.com>
2026-09-10 19:59:29 +0100

    Implement a SessionLocal sessionmaker bound to that engine

--- app/db.py
+++ app/db.py
@@ -1,6 +1,12 @@
 from sqlalchemy import create_engine, Engine
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import sessionmaker
 from .config import settings
 
 def get_engine() -> Engine:
-    engine: Engine = create_engine(settings.database_url)
+    return create_engine(settings.database_url)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=get_engine()
+)
